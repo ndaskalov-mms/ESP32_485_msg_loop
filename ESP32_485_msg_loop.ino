aa7fe4bdf7cbf0fab2959ad45ad97cadb4b0b30a
ndaskalov-mms <ndaskalov@mm-sol.com>
2020-12-28 19:25:25 +0200

    Update ESP32_485_msg_loop.ino

--- ESP32_485_msg_loop.ino
+++ ESP32_485_msg_loop.ino
@@ -26,14 +26,12 @@ constexpr int BITRATE = 115200;
 constexpr int LOG_BITRATE = 115200;
 HardwareSerial& logger(Serial);
 //
-#include "zonen.h"                                                // all zones and pgms related staff
+                                        // all zones and pgms related staff
 //
 #ifdef MASTER
 HardwareSerial& MasterUART(Serial2);
-byte MzoneResult[MASTER_ZONES_CNT/2 + MASTER_ZONES_CNT%2];       //each zone will be in 4bits
 #endif
 #ifdef SLAVE
-byte SzoneResult[SLAVE_ZONES_CNT/2 + SLAVE_ZONES_CNT%2];          //each zone will be in 4bits
 #ifdef LOOPBACK
 HardwareSerial& SlaveUART(Serial1);
 #else
@@ -45,21 +43,28 @@ HardwareSerial& SlaveUART(Serial2);
 //
 #include "mqtt.h"
 #include "errors.h"                   // errors definitions and handling
+#include "zonestest/zonen.h"   
 #include "RS485_cbacks.h"             // callbacks required by RS485 lib and UART related staff (ESP32 specific)
 #include "helpers.h"                  // include helper functions. INCLUDE ONLY AFTER SERIAL PORT DEFINITIONS!!!!
 #include "Alarm_RS485-cpp.h"          // RS485 transport implementation (library)
 //
 // ------------------------- global variables definition -----------------------------
+#ifdef MASTER
+byte MzoneResult[MASTER_ZONES_CNT/2 + MASTER_ZONES_CNT%2];       //each zone will be in 4bits
+#endif
+#ifdef SLAVE
+byte SzoneResult[SLAVE_ZONES_CNT/2 + SLAVE_ZONES_CNT%2];          //each zone will be in 4bits
+#endif
 byte boardID;                             // board ID: master is 0, expanders and others up to 0xE; OxF means bradcast
 int waiting_for_reply = 0;                // tracks current state of the protocol
 int err, retCode;                         // holds error returns from some functions                         
 struct MSG rcvMsg;                        // temp structs for message tr/rcv
 //these are channels to send/receive packets over serial if. The comm to serial is via fRead, fWrite,...
 RS485 MasterMsgChannel (MasterRead, MasterAvailable, MasterWrite, ErrWrite, RxBUF_SIZE);   //RS485 myChannel (read_func, available_func, write_func, msg_len);
-RS485 SlaveMsgChannel  (SlaveRead, SlaveAvailable, SlaveWrite, ErrWrite, RxBUF_SIZE);   //RS485 myChannel (read_func, available_func, write_func, msg_len);
+RS485 SlaveMsgChannel  (SlaveRead, SlaveAvailable, SlaveWrite, ErrWrite, RxBUF_SIZE);      //RS485 myChannel (read_func, available_func, write_func, msg_len);
 //
 #include "protocol.h"                     // send/receive and compse messgaes staff
-
+//
 //
 //  Arduino setup function - call all local setups her
 //
